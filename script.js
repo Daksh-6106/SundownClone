d578e793e6272c4ca3421e5b41320155e2941efd
Daksh-6106 <guptadaksh130@gmail.com>
2025-04-16 17:45:28 +0530

    Finshed page3

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+let elemCont = document.querySelector(".elem-container");
+let fixed = document.querySelector(".fixed-image");
+elemCont.addEventListener('mouseenter', () => {
+    fixed.style.display = "block";
+});
+
+elemCont.addEventListener('mouseleave', () => {
+    fixed.style.display = "none";
+});
+
+let elems = document.querySelectorAll(".elem");
+
+elems.forEach(function(e) {
+    e.addEventListener("mouseenter", () => {
+        const imageInfo = e.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${imageInfo})`;
+    })
+});
